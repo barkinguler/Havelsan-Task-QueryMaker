71c8bc336bee0dc2fb00e234a67ee0949a44b77a
barkinguler <63545411+barkinguler@users.noreply.github.com>
2020-10-09 14:24:42 +0300

    Update Fields.java

--- src/com/company/Fields.java
+++ src/com/company/Fields.java
@@ -1,12 +1,15 @@
 package com.company;
 
-public class Fields implements QueryMethods{
-    String tableName;
-    String pname;
-    String lastName;
-    int age;
-    String position;
-    boolean decision;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fields extends QueryMethods {
+   private String tableName;
+   private String pname;
+   private String lastName;
+   private int age;
+   private String position;
+   private boolean decision;
 
 
     public Fields(String tableName, String pname, String lastName, int age, String position, boolean decision) {
@@ -18,33 +21,30 @@ public class Fields implements QueryMethods{
         this.decision = decision;
     }
 
-    @Override
-    public String OracleInsertStatement() {
-        String query="INSERT INTO "+tableName+" VALUES('"+pname+"','"+lastName+"',"+age+",'"+position+"',"+(decision ? 1 : 0)+");";
-        return query;
+    public String getTableName() {
+        return tableName;
     }
 
-    @Override
-    public String OracleDeleteStatement(String pname) {
-        String query="DELETE FROM "+tableName+" WHERE "+ "pname='"+ pname+"';";
-        return query;
+    public String getPname() {
+        return pname;
     }
 
-    @Override
-    public String OracleUpdateStatement(boolean decision,String pname) {
-        String query= "UPDATE "+tableName+" SET "+ "decision="+(decision ? 1 : 0)+ " WHERE pname='"+pname+"';";
-        return query;
+    public String getLastName() {
+        return lastName;
     }
 
-    @Override
-    public String OracleCreateStatement() {
-        String query="CREATE TABLE "+tableName+"(pname VARCHAR(50),lastName VARCHAR(50), age INTEGER, position VARCHAR(50), decision NUMBER(1) CHECK (decision IN (0,1) ),PRIMARY KEY (pname) );";
-        return query;
+    public int getAge() {
+        return age;
     }
 
-    @Override
-    public String OracleSelectStatement(String pname) {
-        String query="SELECT * FROM "+tableName +" WHERE PNAME='"+pname+"';";
-        return query;
+    public String getPosition() {
+        return position;
     }
+
+    public boolean isDecision() {
+        return decision;
+    }
+
+
+
 }
