4f9f30d19d5cb4d68333dbc07c5924ca36afb9dc
barkinguler <63545411+barkinguler@users.noreply.github.com>
2020-10-09 14:23:33 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	Fields f=new Fields("TABLEE","pname1","lastname",25,"developer",true);
-	System.out.println("Tablo Oluşturma-- "+f.OracleCreateStatement());
-	System.out.println("Ekleme işlemi-- "+f.OracleInsertStatement());
-	System.out.println("Güncelleme işlemi-- "+f.OracleUpdateStatement(false,"pname1"));
-	System.out.println("Silme işlemi-- "+f.OracleDeleteStatement("pname1"));
-    System.out.println("Select işlemi--"+f.OracleSelectStatement("pname1"));
+	System.out.println("Tablo Oluşturma-- "+f.OracleCreateStatement(f.getTableName()));
+	System.out.println("Ekleme işlemi-- "+f.OracleInsertStatement(f.getTableName(),f.getPname(),f.getLastName(),f.getAge(),f.getPosition(),f.isDecision()));
+	System.out.println("Güncelleme işlemi-- "+f.OracleUpdateStatement(f.isDecision(),f.getPname(),f.getTableName()));
+	System.out.println("Silme işlemi-- "+f.OracleDeleteStatement(f.getPname(),f.getTableName()));
+    System.out.println("Select işlemi--"+f.OracleSelectStatement(f.getPname(),f.getTableName()));
+
+
     }
+
+
 }
